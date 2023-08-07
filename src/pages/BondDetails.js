ced59cc43db666cf2402688395f83c4610fb3c6c
Kaustubh Mhatre <kmhatre076@gmail.com>
2023-08-07 18:50:49 +0530

    Made changes in BondDetails

--- src/pages/BondDetails.js
+++ src/pages/BondDetails.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { getBonds } from "../api";
+import BondsNames from "./BondsNames";
 import { Link } from "react-router-dom";
+import "./Bonddetails.css";
+import "../styles/Bonddetails.css";
+// import "../styles/";
 const BondDetails = () => {
-  const bonds = getBonds();
-
+  const bonds = BondsNames();
   const { bondId } = useParams();
   const bond = bonds.find((p) => p.id === parseInt(bondId));
   console.log("in the details");
@@ -16,16 +18,20 @@ const BondDetails = () => {
         <nav>
           <ul>
             <li>
-              <Link to="/dashboard"> Show All Bonds </Link>
+              <Link to="/"> Show All Bonds </Link>
             </li>
           </ul>
         </nav>
       </div>
-      <div>
-        <p>`Bond id is {bondId} `</p>
-        <p>Name : {bond.name}</p>
-        <p>Price : {bond.price}</p>
-        <p>Description : {bond.description}</p>
+      <div className="bond-container">
+        {/* <p>`Bond id is {bondId} `</p> */}
+        <h2 className="bond-title"> {bond.name} Bond Details</h2>
+        <p className="bond-info">Bond Name: {bond.bondName}</p>
+        <p className="bond-info">Bond Price : {bond.price}</p>
+        <p className="bond-info">Issuer: {bond.issuer}</p>
+        <p className="bond-info">Issue Date: {bond.issueDate}</p>
+        <p className="bond-info">Face Value: {bond.faceValue}</p>
+        <p className="bond-info">Description : {bond.description}</p>
       </div>
     </>
   );
