9be93552a2798e83d0fa4544c39caac40a849abc
Kaustubh Mhatre <kmhatre076@gmail.com>
2023-08-07 18:52:06 +0530

    Added Bond.js

---
+++ src/pages/Bond.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import BondsNames from "./BondsNames";
+import BondDetails from "./BondDetails";
+import { Link } from "react-router-dom";
+const Bond = () => {
+  const [selectedBond, setSelectedBond] = useState(null);
+  const bonds = BondsNames();
+  console.log(bonds);
+  const handleBondClick = (bond) => {
+    setSelectedBond(bond);
+  };
+  return (
+    <div>
+      {bonds.map((bond) => (
+        <div key={bond.id} onClick={() => handleBondClick(bond)}>
+          <Link to={`/bonds/${bond.id}`}> View Details </Link>
+          <p>This is the {bond.id} Bond </p>
+        </div>
+      ))}
+
+      {selectedBond && <BondDetails bond={selectedBond} />}
+    </div>
+  );
+};
+export default Bond;
