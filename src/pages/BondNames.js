16d47048a6b509bb942a7fd805892fc42a8e7775
Kaustubh Mhatre <kmhatre076@gmail.com>
2023-08-07 18:53:05 +0530

    Added BondNames

---
+++ src/pages/BondNames.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function BondsNames() {
+  const products = [
+    {
+      id: 1,
+      name: "Bond 1",
+      description: "This is Bond 1",
+      price: 19.99,
+      issuer: "XYZ Corporation",
+      issueDate: "2023-08-07",
+      faceValue: "$1000",
+    },
+    {
+      id: 2,
+      name: "Bond 2",
+      description: "This is Bond 2",
+      price: 29.99,
+      issuer: "XYZ Corporation",
+      issueDate: "2023-08-07",
+      faceValue: "$2000",
+    },
+    {
+      id: 3,
+      name: "Bond 3",
+      description: "This is Bond 3",
+      price: 39.99,
+      issuer: "XYZ Corporation",
+      issueDate: "2023-08-07",
+      faceValue: "$3000",
+    },
+    // Add more products
+  ];
+  return products;
+}
+export default BondsNames;
