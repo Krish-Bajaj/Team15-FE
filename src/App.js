9e7f99a9ec1d0a13d33753c5e1711949e37bfc1f
PIAI21DEC2003 <koushikeduacc3103@gmail.com>
2023-08-08 11:08:21 +0530

    login page css updated

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import { Route, Routes } from "react-router-dom";
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
+import { useAuth0 } from "@auth0/auth0-react";
 
 import BondDetails from "./pages/BondDetails";
 import Dashboard from './pages/Dashboard';
 
+
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
     <>
         <div className="App">
           <LoginButton/>
           <br/>
+          
+          {isAuthenticated ? <Dashboard/> : <></>}
           <LogoutButton/>
           <br />
-          <Profile/>
         </div>
 
       <Routes>
+        {/* <Route path="/" element={<Dashboard/>}/> */}
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route path="/bonds/:bondId" element={<BondDetails />} />
       </Routes>
+
     </>
   );
 }
