00431779ce8e09c0f61fb1e421f51ed7b4644457
Krish Bajaj <53073541+Krish-Bajaj@users.noreply.github.com>
2023-08-08 13:43:57 +0530

    resolves unused variables

--- src/LogoutButton.jsx
+++ src/LogoutButton.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
   return ( isAuthenticated && (
     <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
