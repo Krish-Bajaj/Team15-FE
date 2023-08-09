2137a281fd6886dabd4cf0fcbb2d2a22afe3c588
Kaustubh Mhatre <kmhatre076@gmail.com>
2023-08-09 12:11:46 +0530

    removed comments

--- src/components/ListMain.js
+++ src/components/ListMain.js
@@ -1,7 +1,5 @@
-import { Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import BondDetails from "../pages/BondDetails";
 import axios from "axios";
 
 const ListMain = () => {
@@ -11,67 +9,42 @@ const ListMain = () => {
       .get("http://localhost:8000/")
       .then((response) => {
         // console.log(response.data);
-        // tp = response.data;
         setTp(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
-  const [selectedBond, setSelectedBond] = useState(null);
-  const [isBondClicked, setIsBondClicked] = useState(false);
-  //   console.log(tp);
-  const handleBondClick = (bond) => {
-    setSelectedBond(bond);
-    setIsBondClicked(true);
-    // updateSelectedBond(bond);
-    console.log({ bond });
-  };
 
   return (
     <div className="list">
       <div className="near">
         <h4>Near Maturity Bonds</h4>
         {tp.Near_matured?.map((bond) => (
-          //   <Button variant="outlined" className="btn-space">
-          //     <h3>{bond.issuer}</h3>
-          //   </Button>
-          <div key={bond._id} onClick={() => handleBondClick(bond)}>
+          <div key={bond._id}>
             <Link to={`/bonds/${bond._id}`}> {bond.issuer} </Link>
-            {/* <p>This is the {bond.id} Bond </p> */}
           </div>
         ))}
-        {/* {isBondClicked && <BondDetails props={selectedBond} />} */}
       </div>
 
       <div className="mature" style={{ marginTop: "2rem" }}>
         <h4>Matured Bonds</h4>
 
         {tp.Matured?.map((bond) => (
-          //   <Button variant="outlined" className="btn-space">
-          //     <h3>{bond.issuer}</h3>
-          //   </Button>
-          <div key={bond._id} onClick={() => handleBondClick(bond)}>
+          <div key={bond._id} >
             <Link to={`/bonds/${bond._id}`}> {bond.issuer} </Link>
-            {/* <p>This is the {bond.id} Bond </p> */}
           </div>
-        ))}
-        {/* {isBondClicked && <BondDetails props={selectedBond} />} */}
+        ))}       
       </div>
 
       <div className="not_matured" style={{ marginTop: "2rem" }}>
         <h4>Non Matured Bonds</h4>
 
         {tp.Not_matured?.map((bond) => (
-          //   <Button variant="outlined" className="btn-space">
-          //     <h3>{bond.issuer}</h3>
-          //   </Button>
-          <div key={bond._id} onClick={() => handleBondClick(bond)}>
+          <div key={bond._id} >
             <Link to={`/bonds/${bond._id}`}> {bond.issuer} </Link>
-            {/* <p>This is the {bond.id} Bond </p> */}
           </div>
         ))}
-        {/* {isBondClicked && <BondDetails props={selectedBond} />} */}
       </div>
     </div>
   );
